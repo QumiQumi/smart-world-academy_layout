2ac95efc74490bd0ce3de112efa0061546e7e671
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-10-13 00:40:19 +0700

    make half of home
    without background and images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles/styles.scss";
 import "./components/wrapper/wrapper";
+import "./components/home/home.scss";
 import "./components/main-navbar/main-navbar.scss";
 import "./components/button/button";
-console.log("hi");
