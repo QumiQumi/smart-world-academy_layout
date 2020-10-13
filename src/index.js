eb0a5dc935fe7b284ded209edfe08526c272924a
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-10-13 22:14:58 +0700

    make home
    without adaptive on small and extra large devices

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/styles.scss";
 import "./components/wrapper/wrapper";
+import "./components/header/header.scss";
 import "./components/home/home.scss";
 import "./components/main-navbar/main-navbar.scss";
 import "./components/button/button";
