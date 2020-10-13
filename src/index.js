ecb54b28d7767eda0aaa04021b91b0d1c4416ed9
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-10-14 01:05:32 +0700

    make feauture-panels
    adaptive to small devices

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles/styles.scss";
 import "./components/wrapper/wrapper";
 import "./components/header/header.scss";
-import "./components/home/home.scss";
 import "./components/main-navbar/main-navbar.scss";
+import "./components/home/home.scss";
+import "./components/feature-panels/feature-panels.scss";
+import "./components/panel/panel.scss";
 import "./components/button/button";
