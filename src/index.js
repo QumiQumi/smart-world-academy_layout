6432249d5999804f8ffc7ad8bb36fb15eb8485cc
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-10-14 23:46:01 +0700

    make block how-it-works

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import "./components/main-navbar/main-navbar.scss";
 import "./components/home/home.scss";
 import "./components/feature-panels/feature-panels.scss";
 import "./components/feature-topics/feature-topics.scss";
+import "./components/how-it-works/how-it-works.scss";
+
 // import "./components/panel/panel.scss";
 import "./components/button/button";
