c126f4e28b026f44a17e95b7821c831918cafaec
QumiQumi <glebyan1998@gmail.com>
2020-10-19 17:26:03 +0700

    make footer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import "./components/how-it-works/how-it-works.scss";
 import "./components/pricing-plan/pricing-plan.scss";
 import "./components/trusted-by/trusted-by.scss";
 import "./components/news/news.scss";
+import "./components/trial/trial.scss";
+import "./components/form/form.scss";
+import "./components/footer/footer.scss";
 
-// import "./components/panel/panel.scss";
+
 import "./components/button/button";
